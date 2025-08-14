b5d77e5a4946b0fe91c8971a1e004c4dd33c4ac3
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2025-08-14 17:24:38 +0000

    Deploying to gh-pages from @ rajeshgayathri2003/rajeshgayathri2003.github.io@bfaeb6595d53e4255bebfa4a6ff7c58e61274e72 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,33 +25,12 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
