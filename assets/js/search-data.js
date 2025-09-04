ca263e74876592463a2236a3880a37704b5e0f63
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2025-09-04 19:11:48 +0000

    Deploying to gh-pages from @ rajeshgayathri2003/rajeshgayathri2003.github.io@b9cda4f340933039c29a9d6c96a6fdc61bb73577 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,28 +52,6 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
